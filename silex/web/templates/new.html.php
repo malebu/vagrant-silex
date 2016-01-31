18104919949597d631d7745ca1cb83dd4cbf86dd
buckm <kingmalebu@gmail.com>
2016-01-31 14:20:43 +0100

    build template for to create a blog

--- silex/web/templates/new.html.php
+++ silex/web/templates/new.html.php
@@ -2,14 +2,28 @@
 
 <div class="container">
     <div class="row">
-        <div class="col-xs-12">
-            <div class="jumbotron">
-
-                <h1><strong>Rangler stinkt nach Eiern!</strong></h1>
-                <p>Wurst ist nicht K&auml;se und K&auml;se ist nicht Wurst...</p>
-                <p><a class="btn btn-primary btn-lg" href="#" role="button">
-                        <span class="glyphicon glyphicon-search" aria-hidden="true"></span> Search
-                    </a></p>
+        <div class="container">
+            <div class="row">
+                <div class="col-xs-12">
+                    <div class="panel panel-default panel-body" style ='background-color: #BDBDBD;'>
+                        <form>
+                            <div class="form-group">
+                                <label for="exampleInputEmail1">Email address</label>
+                                <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Email">
+                            </div>
+                            <div class="form-group">
+                                <label for="exampleInputPassword1">Blogbeitrag</label>
+                                <textarea class="form-control" rows="6" placeholder="Gebe hier ein was du bloggen möchtest" ></textarea>
+                            </div>
+                            <div class="checkbox">
+                                <label>
+                                    <input type="checkbox"> Ich möchte das hier wirklich posten </input>
+                                </label>
+                            </div>
+                            <button type="submit" class="btn btn-primary">Absenden</button>
+                        </form>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
